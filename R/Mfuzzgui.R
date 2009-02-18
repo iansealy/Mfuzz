89e7828eb4a91402b5900630f21c6c84d83e4068
m.futschik@biologie.hu-berlin.de <m.futschik@biologie.hu-berlin.de@bc3139a8-67e5-0310-9ffc-ced21a209358>
2009-02-18 15:20:03 +0000

    requirements for Mfuzzgui stated
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Mfuzz@37476 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/Mfuzzgui.R
+++ R/Mfuzzgui.R
@@ -1,4 +1,6 @@
 Mfuzzgui  <- function (){
+require("tcltk") || stop("R-package tcltk needed")
+require("tkWidgets") || stop("Bioconductor-package tkWidgets needed")
 nenv <- new.env(hash = TRUE, parent = parent.frame())
 gui <- tktoplevel()
 tktitle(gui) <- "GUI for Mfuzz package"
